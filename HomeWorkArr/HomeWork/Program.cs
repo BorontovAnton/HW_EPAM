99f14849a003cf535ec4aa0f06c183d16c0f98e5
BorontovAnton <borontov.anton@gmail.com>
2021-04-21 19:25:23 +0300

    First commit

---
+++ HomeWorkArr/HomeWork/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Генерация случайного массива из 10 чисел
+            int[] arr = new int [10];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(-100,100);
+            
+            //Сортировка по увеличению
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            //Вывод массива
+            Console.WriteLine("Массив из 10 чисел отсортированных по увеличению");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
