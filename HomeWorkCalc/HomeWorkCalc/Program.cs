01d028e8d293956be08818efc613b0e7c2df3a07
BorontovAnton <borontov.anton@gmail.com>
2021-04-21 19:29:31 +0300

    Stcond commit

---
+++ HomeWorkCalc/HomeWorkCalc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HomeWorkCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = 0; double j = 0;
+
+            Console.WriteLine("Калькулятор\r");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Введите первое число и нажмите Enter");
+            i = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число и нажмите Enter");
+            j = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Выберите метематическое действие:");
+            Console.WriteLine("+ - Сумма");
+            Console.WriteLine("- - Разница");
+            Console.WriteLine("* - Умножение");
+            Console.WriteLine("/ - Деление");
+            Console.Write("Введите знак математического действия ");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"Результат вычисления: {i} + {j} = " + (i + j));
+                    break;
+                case "-":
+                    Console.WriteLine($"Результат вычисления: {i} - {j} = " + (i - j));
+                    break;
+                case "*":
+                    Console.WriteLine($"Результат вычисления: {i} * {j} = " + (i * j));
+                    break;
+                case "/":
+                    while (j == 0) {
+                        Console.WriteLine("Введите число неравное 0");
+                        j = Convert.ToDouble(Console.ReadLine());
+                    }
+                    Console.WriteLine($"Результат вычисления: {i} / {j} = " + (i / j));
+                    break;
+            }
+            
+            Console.Write("Нажмите любую кнопку для закрытия приложения");
+            Console.ReadKey();
+        }
+    }
+}
